5f2a2dbec879efe48456cfed4747906988e609fe
张卿 <2803446448@qq.com>
2023-11-04 00:07:03 +0800

    Time Difference

--- test9_24.cpp
+++ test9_24.cpp
@@ -1,7 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+
 int isLeep(int year) {
-	if (year / 4 == 0 && year / 100 != 0 || year / 400 == 0) {
+	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
 		return 1;
 	}
 
