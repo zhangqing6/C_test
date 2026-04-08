cb3db3bdc2992e51f698368be2e1f45ab91d6d5a
zhangqing6 <141533401+zhangqing6@users.noreply.github.com>
2026-04-09 00:24:22 +0800

    productExceptSelf

--- productExceptSelf.cpp
+++ productExceptSelf.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> pre(n, 1);   // 前缀乘积
-        vector<int> suf(n, 1);   // 后缀乘积
-        vector<int> ans(n);
-
-        // 1. 计算前缀：pre[i] = nums[0]~nums[i-1] 的乘积
-        for (int i = 1; i < n; i++) {
-            pre[i] = pre[i-1] * nums[i-1];
+        int n=nums.size();
+        vector<int>ans(n,1);
+        for(int i=1;i<n;i++){
+            ans[i]=ans[i-1]*nums[i-1];
         }
-
-        // 2. 计算后缀：suf[i] = nums[i+1]~nums[n-1] 的乘积
-        for (int i = n-2; i >= 0; i--) {
-            suf[i] = suf[i+1] * nums[i+1];
+        int suf=1;
+        for(int i=n-1;i>=0;i--){
+            ans[i]=ans[i]*suf;
+            suf*=nums[i];
         }
-
-        // 3. 结果 = 左 × 右
-        for (int i = 0; i < n; i++) {
-            ans[i] = pre[i] * suf[i];
-        }
-
         return ans;
     }
 };
